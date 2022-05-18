05feb9ab55893fe7509c7715f9a9941c635eefb7
ugurcan00 <ugurcanngull@gmail.com>
2022-05-18 13:35:23 +0300

    ASSAS

--- MigrationScript.py
+++ MigrationScript.py
@@ -30,7 +30,10 @@ base_tables = [
     "MEVCUT_METROBUS_HATTI",
     "PLN_RAYLI_SISTEM_HAT",
     "MEVCUT_RAYLI_SISTEM_HATLARI",
-    "DURAK_AKTARMA_NOKTALARI"]
+    "DURAK_AKTARMA_NOKTALARI",
+    "DURAK_DETAY"]
+
+
 gdb_path = r'C:\YAYIN\PG\BaseTables.gdb'
 sde_path = r"C:\YAYIN\PG\sde_gyy.sde"
 itrf_96 = 'PROJCS["ITRF96 / TM30",GEOGCS["GCS_ITRF_1996",DATUM["D_ITRF_1996",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["false_easting",500000.0],PARAMETER["false_northing",0.0],PARAMETER["central_meridian",30.0],PARAMETER["scale_factor",1.0],PARAMETER["latitude_of_origin",0.0],UNIT["m",1.0]]'
