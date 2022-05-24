17b6a198bcc33c9254520f9ee8ab6fc821b9a910
kuttamuwa <ucok.umut@gmail.com>
2022-05-24 20:50:58 +0300

    MigrationScript.py düzenlendi

--- Scripts/prodviews/Report_2.sql
+++ Scripts/prodviews/Report_2.sql
@@ -3,7 +3,5 @@ d.DURAK_KODU AS DURAK_KODU, CAST(ROW_NUMBER() OVER (ORDER BY d.ESKI_DURAK_ID) AS
 dd.O1, dd.O2, dd.O3, dd.O4, 
 d.SHAPE 
 FROM SDE.DURAK d 
-INNER JOIN SDE.DURAK_DETAY dd ON d.ESKI_DURAK_ID = dd.DURAK_ID 
-
-/*durak detay tablosu base katman olacak. durakların kaldırım vb. gibi kısımlara olan mesafeleri yer almaktadır.
-BASE TABLOLARDAN ÇÖZÜMLENİYOR.
+INNER JOIN SDE.DURAK_DETAY dd ON d.ESKI_DURAK_ID = dd.DURAK_ID
+--BITTI
