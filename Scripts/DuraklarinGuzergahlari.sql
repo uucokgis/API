c5397ee0ffcd54ad618cb9e5e4bc7a74acdee4a0
kuttamuwa <ucok.umut@gmail.com>
2022-05-25 16:50:41 +0300

    DuraklarinGuzergahlari.sql

--- Scripts/DuraklarinGuzergahlari.sql
+++ Scripts/DuraklarinGuzergahlari.sql
@@ -1,49 +1,23 @@
---GYY .NET Reports 
+--GYY .NET Reports
 
-/* 
- Report 2: 
- Duraklarýn Güzergahlarý :
- 
- * UYARI
- * Ben bunu GYY uygulamasindaki rapor karsiliklarinda bulamadim.
- * Asagidaki SQLi koddan cektim. Anlasilan, shape isterse sql calisiyor
- * TODO: StopRoute objesini sezerden ogrenmek lazim, kodun baska bi yerinde de gecmiyor.
- 
- * 
-**/  
+/*  Report 3:  Duraklarin Guzergahlari Raporu: ** View Name: VW_DURAKGUZERGAH
 
--- Report icin SQL
-SELECT * 
+*/
 
-FROM GUZERGAH g
-INNER JOIN GUZERGAH_SEGMENT gs ON g.ID = gs.GUZERGAH_ID
-INNER JOIN SEGMENT s ON s.ID = gs.SEGMENT_ID
+-- POSTGRESQL: SDE-- DURAK ADI, DURAK KODU, GUZERGAH KODU, HAT KODU,-- DURAKTAN GECEN HAT SAYISI
 
-WHERE d.DURAK_KODU > 0
-ORDER BY d.DURAK_KODU
-
-
--- Shape icin SQL
-SELECT  D.DURAK_KODU,D.ID,D.ADI, G.GUZERGAH_KODU, D.GEOLOC, 
-(SELECT COUNT (*) FROM HAT H WHERE H.ID=G.HAT_ID) AS HATSAYISI 
-
--- tablolar 
-FROM GUZERGAH G 
-JOIN GUZERGAH_SEGMENT GS ON G.ID = GS.GUZERGAH_ID 
-JOIN SEGMENT S ON GS.SEGMENT_ID = S.ID 
-JOIN DURAK  D ON D.ID = S.BI_DURAK_ID 
-
--- filtreler
-WHERE S.TIP = 3 
-
--- Bu araya firlamis ne ise yariyor hic belli degil !?
-UNION ALL SELECT  D.DURAK_KODU,D.ID,D.ADI, G.GUZERGAH_KODU, D.GEOLOC, 
-(SELECT COUNT(*) FROM HAT H WHERE H.ID = G.HAT_ID ) AS HATSAYISI 
-FROM GUZERGAH G 
-JOIN GUZERGAH_SEGMENT GS ON G.ID = GS.GUZERGAH_ID 
-JOIN SEGMENT S ON GS.SEGMENT_ID = S.ID  
-JOIN DURAK D ON D.ID = S.BA_DURAK_ID 
-
--- filtreler
-WHERE S.TIP = 1
+CREATE OR REPLACE VIEW VW_DURAKGUZERGAH AS
+SELECT d.durak_kisa_adi,
+       d.durak_kodu,
+       g.guzergah_kodu,
+       g.hat_kodu,
+       att.dghs as
+           "DURAKTAN GECEN HAT SAYISI"
+from guzergah_segment gsjoin segment s on s.globalid = gs.segment_guid
+    join durak d on d.DURAK_ID = s.ba_durak_id
+    join guzergah g on g.ID = gs.GUZERGAH_ID
+    join (select count(g.hat_id) as dghs, g.ID, g.hat_id from guzergah_segment gs
+    join segment s on s.globalid = gs.segment_guid
+    join durak d on d.DURAK_ID = s.ba_durak_id
+    join guzergah g on g.ID = gs.GUZERGAH_IDgroup by g.hat_id, g.ID) as att on att.hat_id = g.hat_id
 
