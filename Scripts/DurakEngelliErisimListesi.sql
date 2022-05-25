81d7cc1b9d8996ab6c3327693930bc9a4b118821
kuttamuwa <ucok.umut@gmail.com>
2022-05-25 16:29:43 +0300

    engelli erişim listesi + duraklar arası mesae süre tamam gibi

--- Scripts/DurakEngelliErisimListesi.sql
+++ Scripts/DurakEngelliErisimListesi.sql
@@ -2,7 +2,7 @@
 
 /* 
  Report 1: 
- Duraklar i�in engelli erisim listesi:
+ Duraklar için engelli erisim listesi:
  ** View Name: VW_DURAKERISIMLISTESI
  
   * EK ISLEMLER: 
@@ -10,20 +10,20 @@ var _sart1 = Math.Max((Math.Min(item.StopDetail.O1, item.StopDetail.O3)), (Math.
 var _sart2 = item.Stop.ENGELLI_RAMPA;
 
 sart 1'de : Yani, Max (  Min(O1, O3) ve Min(O2, O4)  ) bulunur.
-sart 2'de ENGELLI_RAMPA'ya bak�l�r == 1 ise engelli rampa vard�r.
+sart 2'de ENGELLI_RAMPA'ya bakýlýr == 1 ise engelli rampa vardýr.
 
 _sart1 > 1.2  &&  _sart2: 1 -> uygun
-bu durum saglanmiyorsa _neden doldurulur: Rapor ciktisinda uygunsuzluk nedeni s�tunu.
+bu durum saglanmiyorsa _neden doldurulur: Rapor ciktisinda uygunsuzluk nedeni sütunu.
 
 * UYARI
-!! X, Y s�tunlar�nda 7932'den wgs84'e koordinat d�n�s�m� var.
-	** Z sutunu  i�in kodda:
+!! X, Y sütunlarýnda 7932'den wgs84'e koordinat dönüsümü var.
+	** Z sutunu  için kodda:
 		var Z = yardimcirampa.FirstOrDefault(x => x.VALUE == item.StopDetail.Z);
 		Z = Z == null ? "" : Z.TEXT
-		Raporda ise "Var", "Yok" ve "" yaz�yor.
+		Raporda ise "Var", "Yok" ve "" yazýyor.
 		
  _durum: Rapor ciktisinda ENGELLI ERISIME UYGUNLUK
-Engelli rampa "" ise null yazilir: Bu muhtemelen styling i�in
+Engelli rampa "" ise null yazilir: Bu muhtemelen styling için
 X, Y sutunlarinda kodda niye Centroid() var anlamadim.
 
 */  
@@ -35,13 +35,19 @@ X, Y sutunlarinda kodda niye Centroid() var anlamadim.
 */
 -- SDO:
 
-CREATE OR REPLACE VIEW SDE.VW_DURAKERISIMLISTESI AS SELECT d.DURAK_ID AS ESKI_DURAK_ID, d.ADI AS DURAK_ADI,
-cast(ROW_NUMBER() OVER (ORDER BY d.DURAK_ID) as INTEGER) AS ROW_ID,
-d.DURAK_KODU AS DURAK_KODU, d.OBJECTID,
-ii.ILCEADI AS ILCE_ADI, m.ADI AS MAHALLE_ADI, dd.K, d.ENGELLI_RAMPA,
-dd.O1, dd.O2, dd.O3, dd.O4,
-d.YON_BILGISI, d.KALDIRIM_GENISLIGI, dd.Z,  d.SHAPE, SDE.ST_X(d.SHAPE) AS X, SDE.ST_Y(d.SHAPE) AS Y
-FROM SDE.DURAK d
-INNER JOIN SDE.DURAK_DETAY dd ON d.DURAK_ID = dd.DURAK_ID
-LEFT JOIN SDE.ILCELER ii ON ii.TUIK_ILCE_KODU = d.ILCEID
-LEFT JOIN SDE.MAHALLELER m ON m.TUIK_MAHALLE_KODU = d.MAHALLEID;
+CREATE OR REPLACE VIEW VW_DURAKERISIMLISTESI AS 
+
+-- POSTGRESQL: SDE
+-- DURAKID, DURAK ADI. DURAK KODU, ENGELLI ERISIME
+-- UYGUNLUK, UYGUNSUZLUK NEDENI, O1, O2, O3, O4. K(m), ILCE ADI, MAHALLE ADI,
+-- ENGELLI RAMPASI, YON BILGISI, KALDIRIM GENISLIGI, ENLEM, BOYLAM
+select d.DURAK_ID, d.durak_kisa_adi, d.durak_kodu, dd.o1, dd.o2, dd.o3, dd.o4, dd.k,i.ilceadi,
+m.adi as mahalle_adi, db.kaldirim_genisligi, db.kaldirim_genisligi, 
+split_part(sde.st_astext(d.shape)::TEXT, ' ', 3) as ENLEM,
+split_part(sde.st_astext(d.shape)::TEXT, ' ', 4) as BOYLAM
+-- engelli erisime uygunluk, uygunsuzluk nedeni,yon_bilgisi 
+from durak d 
+join durak_detay dd on d.GLOBALID = dd.DURAK_GUID
+join ilceler i on i.tuik_ilce_kodu = d.ilceid 
+join mahalleler m on m.tuik_mahalle_kodu = d.mahalleid 
+join durak_bilgi db on db.durak_guid = d.globalid
--- Scripts/DuraklarArasiMesafeSure.sql
+++ Scripts/DuraklarArasiMesafeSure.sql
@@ -1,33 +1,28 @@
---GYY .NET Reports 
+--GYY .NET Reports   /*  Report 4: Duraklar Arasi Mesafe Suresi  Raporu: ** View Name: VW_DURAKSUREMESAFE
+-- --TESTHATYONETIM GUZERGAH: 54099 GUZERGAH_GEOLOC: 41394 GUZERGAH_GEOLOC_MAP: 4456
+-- --HATYONETIM GUZERGAH: 54939 GUZERGAH_GEOLOC: 227066 GUZERGAH_GEOLOC_MAP: 4456
+--
+-- GUZERGAH_SEGMENT (TABLE) : GUZERGAH_GUID, SEGMENT_GUID, SIRA_NO
+-- SEGMENT: BA_DURAK_GUID, BI_DURAK_GUID, GLOBALID, SHAPE  HAT KODU, GÜZERGAH KODU, BAŞLANGIÇ DURAK ADI,
+-- BİTİŞDURAK ADI, SIRA, MESAFE(KM), SÜRE(DK), BADIJRAKID, BİDURAKID */-- POSTGRESQL: SDE-- BITMEK UZERE
 
-/* 
- Report 4:
- Duraklar Arasi Mesafe Suresi  Raporu:
- ** View Name: VW_DURAKSUREMESAFE
 
-
- --TESTHATYONETIM
- GUZERGAH: 54099
- GUZERGAH_GEOLOC: 41394
- GUZERGAH_GEOLOC_MAP: 4456
-
- --HATYONETIM
- GUZERGAH: 54939
- GUZERGAH_GEOLOC: 227066
- GUZERGAH_GEOLOC_MAP: 4456
-
- GUZERGAH_SEGMENT (TABLE) : GUZERGAH_GUID, SEGMENT_GUID, SIRA_NO
- SEGMENT: BA_DURAK_GUID, BI_DURAK_GUID, GLOBALID, SHAPE
-
- */
--- POSTGRESQL: SDE
--- BITMEK UZERE
-
-select gs.SIRA, g.GUZERGAH_ADI, g.GUZERGAH_KODU, g.HAT_KODU, g.HAT_ID, g.SHAPE, d.DURAK_KODU,
-d.DURAK_KISA_ADI, d.DURAK_ID,   -- BASLANGIC DURAK KODU, BITIS DURAK KODU YANYANA YAZMAK ICIN COZUM LAZIM
-us.mesafe, us.sure
+select g.hat_kodu,
+       g.guzergah_kodu,
+       d.durak_kisa_adi  as bas_durak_adi,
+       dd.durak_kisa_adi as bit_durak_adi,
+       gs.sira,
+       us.mesafe,
+       us.sure,
+       d.durak_id        as bas_durak_id,
+       dd.durak_id       as bit_durak_id
 from GUZERGAH_SEGMENT gs
-join SEGMENT us on us.ID = gs.SEGMENT_ID
-join GUZERGAH g on g.ID = gs.GUZERGAH_ID
-join DURAK d on d.DURAK_ID = us.BA_DURAK_ID or US.BI_DURAK_ID = d.DURAK_ID
-WHERE gs.GUZERGAH_ID = 24401 AND gs.SEGMENT_ID = 1851;
+         join SEGMENT us
+              on us.ID = gs.SEGMENT_ID
+         join GUZERGAH g
+              on g.ID = gs.GUZERGAH_ID
+         left join DURAK d on d.DURAK_ID = us.BA_DURAK_ID
+         left join DURAK dd on dd.DURAK_ID = us.BI_DURAK_ID
+WHERE gs.GUZERGAH_ID = 24401
+  AND gs.SEGMENT_ID = 1851;
+
