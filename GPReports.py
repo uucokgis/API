d120ea438a262c0ac668f7947f7c061f8351b744
kuttamuwa <ucok.umut@gmail.com>
2022-05-25 22:27:43 +0300

    GPReports.py based

--- GPReports.py
+++ GPReports.py
@@ -1,4 +1,37 @@
+import os.path
+
 import arcpy
+from arcpy import AddMessage as msg
+import pandas as pd
+
+REPORT_ITEMS = {
+    "Durak Cephe Ölçüleri": "VW_DURAKCEPHEOLCULERI",
+    "Duraklar Arası Mesafe Süre": None,
+    "Duraklar için Engelli Erişim Listesi": None,
+    "Duraklar": None,
+    "Durakların Güzergah Raporu": None,
+    "Fotoğraf Olmayan Durak Raporu": None,
+    "Garajlar Raporu": None,
+    "Güzergah Ölü KM Raporu": None,
+    "Güzergahların ilk Son Durak Raporu": None,
+    "Güzergahsız Durak Raporu": None,
+    "Güzergahtan Bağımsız Segmentler Raporu": None,
+    "Güzergahlar Raporu": None,
+    "Hat Başı Hat Sonu Raporu": None,  # şartnamede var appde yok
+    "Hatlar Raporu": None,
+    "BA Raporu": None,
+    # "Proje Alanı Raporu",
+    # "İlçe Kartı Raporu",
+    "Koordinatlı Hat Durak Sıra Liste Raporu": None,
+    "Peronlar Raporu": None,
+    # "Güzergah Değişim Raporu",
+    # "Durak Değişim Raporu"
+
+}
+REPORTS_FOLDER = r"C:\YAYIN\GP_REPORT_OUTPUTS"
+
+SDE_PATH = r"C:\YAYIN\PG\sde_gyy.sde"
+DB_SCHEMA = "gyy.sde."
 
 
 class Toolbox(object):
@@ -22,11 +55,11 @@ class ReportGenerator(object):
     def getParameterInfo(self):
         """Define parameter definitions"""
         report_param = arcpy.Parameter(
-            displayName="Rapor",
+            displayName="Rapor Tipi",
             datatype="GPString",
             name="Rapor",
             parameterType="Required",
-            direction="Input"
+            direction="Input",
         )
 
         out_excel = arcpy.Parameter(
@@ -37,30 +70,8 @@ class ReportGenerator(object):
             direction="Output"
         )
 
-        report_param.filter.list = [
-            "Durak Cephe Ölçüleri",
-            "Duraklar Arası Mesafe Süre",
-            "Duraklar için Engelli Erişim Listesi",
-            "Duraklar",
-            "Durakların Güzergah Raporu",
-            "Fotoğraf Olmayan Durak Raporu",
-            "Garajlar Raporu",
-            "Güzergah Ölü KM Raporu",
-            "Güzergahların ilk Son Durak Raporu",
-            "Güzergahsız Durak Raporu",
-            "Güzergahtan Bağımsız Segmentler Raporu",
-            "Güzergahlar Raporu",
-            "Hat Başı Hat Sonu Raporu",  # şartnamede var appde yok
-            "Hatlar Raporu",
-            "BA Raporu",
-            # "Proje Alanı Raporu",
-            # "İlçe Kartı Raporu",
-            "Koordinatlı Hat Durak Sıra Liste Raporu"
-            "Peronlar Raporu",
-            # "Güzergah Değişim Raporu",
-            # "Durak Değişim Raporu"
-
-        ]
+        report_param.filter.type = "ValueList"
+        report_param.filter.list = list(REPORT_ITEMS.keys())
 
         parameters = [report_param, out_excel]
         return parameters
@@ -78,4 +89,14 @@ class ReportGenerator(object):
 
     def execute(self, parameters, messages):
         """The source code of the tool."""
-        return
+        report_name = parameters[0].valueAsText
+        report_output = os.path.join(REPORTS_FOLDER, report_name)  # CAUTION: no extension
+        msg("Report output : " + report_output)
+
+        related_view = REPORT_ITEMS[report_output]
+        related_view_path = os.path.join(SDE_PATH, DB_SCHEMA + related_view)
+        msg("Related view : " + related_view)
+        msg("Related view path : " + related_view_path)
+
+        df = pd.DataFrame.spatial.from_featureclass(related_view_path)
+        msg("First 5 Rows: \n" + df.head())
