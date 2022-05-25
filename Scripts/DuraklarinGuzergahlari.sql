0dedea9adc3aaa387e70c22cb8492181ee198a07
kuttamuwa <ucok.umut@gmail.com>
2022-05-25 18:19:11 +0300

    GuzergahIlkSonDurakRaporu.sql + DuraklarinGuzergahlari.sql

--- Scripts/DuraklarinGuzergahlari.sql
+++ Scripts/DuraklarinGuzergahlari.sql
@@ -7,17 +7,33 @@
 -- POSTGRESQL: SDE-- DURAK ADI, DURAK KODU, GUZERGAH KODU, HAT KODU,-- DURAKTAN GECEN HAT SAYISI
 
 CREATE OR REPLACE VIEW VW_DURAKGUZERGAH AS
-SELECT d.durak_kisa_adi,
+
+SELECT d.durak_kisa_adi, d.durak_kodu, g.guzergah_kodu, g.hat_kodu, g.ID
+-- DURAKTAN GECEN HAT SAYISI
+from guzergah_segment gs
+join segment s on s.globalid = gs.segment_guid
+join durak d on d.DURAK_ID = s.ba_durak_id
+join guzergah g on g.ID = gs.GUZERGAH_ID where g.ID = 3162
+
+----------------------
+select d.durak_kodu, s.ID as s1id, s2.ID as s2id, s.ba_durak_id, s2.bi_durak_id, gs.guzergah_id, g.ID, g.hat_id, g.hat_kodu from durak d
+join segment s on s.ba_durak_id = d.ID
+join segment s2 on s2.bi_durak_id = d.ID
+join guzergah_segment gs on gs.segment_id = s.ID
+join guzergah g on g.ID = gs.guzergah_id
+
+-----------------
+SELECT g.ID, d.durak_kisa_adi,
        d.durak_kodu,
        g.guzergah_kodu,
        g.hat_kodu,
        att.dghs as
            "DURAKTAN GECEN HAT SAYISI"
-from guzergah_segment gsjoin segment s on s.globalid = gs.segment_guid
+from guzergah_segment gs join segment s on s.globalid = gs.segment_guid
     join durak d on d.DURAK_ID = s.ba_durak_id
     join guzergah g on g.ID = gs.GUZERGAH_ID
     join (select count(g.hat_id) as dghs, g.ID, g.hat_id from guzergah_segment gs
     join segment s on s.globalid = gs.segment_guid
     join durak d on d.DURAK_ID = s.ba_durak_id
-    join guzergah g on g.ID = gs.GUZERGAH_IDgroup by g.hat_id, g.ID) as att on att.hat_id = g.hat_id
-
+    join guzergah g on g.ID = gs.GUZERGAH_ID group by g.hat_id, g.ID, d.DURAK_ID) as att on att.hat_id = g.hat_id
+where g.ID = 3162
---
+++ Scripts/GuzergahIlkSonDurakRaporu.sql
@@ -0,0 +1,26 @@
+--GYY .NET Reports
+
+/*
+ Report 12:
+ Guzergahin ilk son durak raporu:
+ ** View Name: VW_GUZ_ILKSON_DURAK_VW
+ * XA, XB, YA, YB, KESITKODU?, DURAKKODUA, DURAKKODUB, MESAFE, SURE
+ *
+
+
+*/
+
+-- POSTGRESQL: SDE
+select gs.guzergah_id, d.durak_kodu as basdurakkodu,
+d2.durak_kodu as bitdurakkodu, att.maxsira, att.minsira
+from guzergah_segment gs
+join (
+SELECT gs.guzergah_id, max(sira) as maxsira, min(sira) as minsira FROM guzergah_segment gs
+join segment s on s.ID = gs.segment_id
+group by gs.guzergah_id) as att on att.guzergah_id = gs.guzergah_id
+join segment s2 on s2.ID = gs.segment_id
+join durak d on d.DURAK_ID = s2.ba_durak_id
+join durak d2 on d2.DURAK_ID = s2.bi_durak_id
+where gs.guzergah_id = 23713 and
+(gs.sira = maxsira or gs.sira = minsira)
+group by d.durak_kodu, gs.guzergah_id, d2.durak_kodu, att.maxsira, att.minsira
