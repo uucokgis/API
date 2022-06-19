620a18a8fd5fe236c4390fdeb5919b048d5c5df2
kuttamuwa <NeDved.11901>
2022-06-19 15:39:50 +0300

    ba raporu ok hb garaj hs garaj testing

--- DortTableScripts/tests/tests.py
+++ DortTableScripts/tests/tests.py
@@ -33,11 +33,17 @@ class DortTableTests(TestCase):
         print(f"Gecen zaman : {end_time - start_time} saniye")
         assert arcpy.Exists(ba_report_out)
 
-    def test_durak_baslangic_garaj_rota(self):
+    @staticmethod
+    def durak_garaj_rota(_type='BASLANGIC'):
         start_time = time.time()
-        hbas_garaj_view = os.path.join(SDE_PATH, f"{DB_SCHEMA}.view_durak_hs_garaj")
-        hbas_report_out = os.path.join(SDE_PATH, f"{DB_SCHEMA}.durakhb_garaj_rota")
-        hbas_report_df = table_to_data_frame(hbas_garaj_view)
+        if _type == 'BASLANGIC':
+            durak_garaj_view = os.path.join(SDE_PATH, f"{DB_SCHEMA}.view_durak_hb_garaj")
+            durak_report_out = os.path.join(SDE_PATH, f"{DB_SCHEMA}.durakhb_garaj_rota")
+        else:  # BITIS
+            durak_garaj_view = os.path.join(SDE_PATH, f"{DB_SCHEMA}.view_durak_hs_garaj")
+            durak_report_out = os.path.join(SDE_PATH, f"{DB_SCHEMA}.durakhs_garaj_rota")
+
+        hbas_report_df = table_to_data_frame(durak_garaj_view)
         hbas_report_df.rename(columns={
             "durak_x": "from_x",
             "durak_y": "from_y",
@@ -48,30 +54,14 @@ class DortTableTests(TestCase):
         rg.concurrent_road_generator()
         rg.road_to_gdf()
 
-        rg.df.spatial.to_featureclass(hbas_report_out, overwrite=True)
+        rg.df.spatial.to_featureclass(durak_report_out, overwrite=True)
         end_time = time.time()
         print(f"Gecen zaman : {end_time - start_time} saniye")
-        assert arcpy.Exists(hbas_report_out)
+        assert arcpy.Exists(durak_report_out)
 
-    def test_durak_bitis_garaj_rota(self):
-        start_time = time.time()
-        hbit_garaj_view = f"{DB_SCHEMA}.durakhs_garaj_view"
-        hbit_report_out = os.path.join(SDE_PATH, f"{DB_SCHEMA}.durakhs_garaj_rota")
-        hbit_report_df = table_to_data_frame(hbit_garaj_view)
-        hbit_report_df.rename(columns={
-            "durak_x": "from_x",
-            "durak_y": "from_y",
-            "garaj_x": "to_x",
-            "garaj_y": "to_y"
-        }, inplace=True)
-        rg = RoadGenerator(hbit_report_df, oid='row_id')
-        rg.concurrent_road_generator()
-        rg.road_to_gdf()
-
-        rg.df.spatial.to_featureclass(hbit_report_out, overwrite=True)
-        end_time = time.time()
-        print(f"Gecen zaman : {end_time - start_time} saniye")
-        assert arcpy.Exists(hbit_report_out)
+    def test_durak_garaj_rota(self):
+        self.durak_garaj_rota()
+        self.durak_garaj_rota(_type='BITIS')
 
     def test_gar_durak_rota(self):
         raise NotImplementedError
